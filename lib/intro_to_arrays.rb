17fbf77a0a816d22812df0d3ae9bb5c51d770506
David Foote <david.a.foote@gmail.com>
2019-06-05 03:45:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = [1, 2]
+end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
